e43babb83b4e6a0b95cb79240dae524c418df895
Shubham Kaushal <47340042+ShubhamKaushal15@users.noreply.github.com>
2022-03-10 18:37:53 -0800

    fixed write mode

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -15,7 +15,7 @@ def save_data(df, outpath):
     
     for k, v in df_dict.items():
         if k.startswith("dim"): # dim cols are saved as .json
-            with open(os.path.join(outpath, k+".json"), 'w') as outfile:
+            with open(os.path.join(outpath, k+".json"), 'w+') as outfile:
                       json.dump(v.to_json(), outfile)
         else: # sleep eff columns are saved as csv
             v.to_csv(os.path.join(outpath, k+".csv"), index = False)
